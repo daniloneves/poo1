056297c70778d08daf63cfb5f11ebcb5eb2f2cc5
Danilo Lustosa <danilolustosa@gmail.com>
2020-07-09 16:59:09 -0300

    First commit

---
+++ ContaBancaria.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO1
+{
+    class ContaBancaria
+    {
+        public string Conta { get; set; }
+        public string Agencia { get; set; }
+        public string Nome { get; set; }
+        public string Senha { private get; set; }
+        private int Saldo;
+
+        public ContaBancaria(int saldo)
+        {
+            Saldo = saldo;
+        }
+
+        public void Depositar(int valor)
+        {
+            Saldo = Saldo + valor;
+        }
+
+        public void Sacar(int valor)
+        {
+            Saldo = Saldo - valor;
+        }
+
+        public int ConsultarSaldo()
+        {
+            int taxa = 10;
+            int valorTaxa = (Saldo * taxa) / 100;
+            return Saldo - valorTaxa;
+        }
+
+        public void ImprimirSaldo() 
+        {
+            Console.WriteLine($"{Nome}, o saldo da sua conta {Conta} na agência {Agencia} é: {ConsultarSaldo()}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace POO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria = new ContaBancaria(200);
+
+            contaBancaria.Agencia = "1234-5";
+            contaBancaria.Conta = "179845-6";
+            contaBancaria.Nome = "Danilo Lustosa";
+            contaBancaria.Senha = "senha123";
+
+            contaBancaria.ImprimirSaldo();
+            
+
+        }
+    }
+}
